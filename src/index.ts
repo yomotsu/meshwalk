e10c35c61dd7e14aa9ece4f6b798b615ccbc20b2
Akihiro Oyamada <admin@yomotsu.net>
2026-08-22 15:16:03 +0900

    feat: serialize and hydrate static octrees

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { World } from './core/World';
 export { Body } from './core/Body';
 export { StaticBody } from './core/StaticBody';
+export type { SerializedOctree } from './core/Octree';
 export { KinematicBody } from './core/KinematicBody';
 export { CharacterController, type CharacterControllerOptions, type CharacterControllerEventType } from './core/CharacterController';
 export { ClimbableBody, type ClimbableBodyOptions, type ClimbableMode } from './core/ClimbableBody';
